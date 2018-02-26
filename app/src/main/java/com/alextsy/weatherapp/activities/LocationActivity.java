71e3871f8442c724ee87d7137ff596226e7fb2db
AlexTsybin <alexander.tsybin@gmail.com>
2018-02-26 22:38:12 +0300

    Widget specifies city by saved user location (lat, long)

---
+++ app/src/main/java/com/alextsy/weatherapp/activities/LocationActivity.java
@@ -0,0 +1,119 @@
+package com.alextsy.weatherapp.activities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.alextsy.weatherapp.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import android.Manifest;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by os_mac on 25.02.18.
+ */
+
+public class LocationActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPref;
+
+    private TextView curr_lat;
+    private TextView curr_long;
+    private TextView curr_city;
+    private Button get_lat_long;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_activity);
+
+        sharedPref = getSharedPreferences(
+                String.valueOf(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        curr_lat = findViewById(R.id.current_lat);
+        curr_long = findViewById(R.id.current_long);
+        curr_city = findViewById(R.id.current_geo_city);
+        get_lat_long = findViewById(R.id.get_lat_long);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        get_lat_long.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayLocation();
+            }
+        });
+
+    }
+
+    private void displayLocation() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    0);
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            double latD = location.getLatitude();
+                            double longD = location.getLongitude();
+                            curr_lat.setText(String.valueOf(latD));
+                            curr_long.setText(String.valueOf(longD));
+
+                            String city = getCityFromLocation(latD, longD);
+
+                            curr_city.setText(city);
+
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putString(String.valueOf(R.string.saved_city), city);
+                            editor.apply();
+                        }
+                    }
+                });
+
+    }
+
+    @Nullable
+    private String getCityFromLocation(double latitude, double longtitude) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longtitude, 1);
+
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                String city = returnedAddress.getLocality();
+                return city;
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
